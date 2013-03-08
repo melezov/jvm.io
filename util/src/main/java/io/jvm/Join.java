789d492624f3e3d07820b0a3ec0f28229e465db5
Marko Elezović <marko@element.hr>
2013-03-08 08:18:40 +0100

    Formatted Join, made it "generic".

--- util/src/main/java/io/jvm/Join.java
+++ util/src/main/java/io/jvm/Join.java
@@ -1,177 +1,211 @@
 package io.jvm;
 
 public class Join {
+    public static final String escaping(
+            final String uris[],
+            final char separator) {
 
-  public static final String escaping(final String uris[], final char separator) {
+        // escaping is a stable function
+        if (uris == null) return null;
 
-    if (uris == null) return null;
-    if (uris.length==0) throw new IllegalArgumentException("URI list cannot be empty");;
+        int cnt = uris.length; // number of separators between uris
+        if (cnt == 0) throw new IllegalArgumentException("URI list cannot be empty");
 
-    int charCnt = uris.length-1; //number of separators between uris
+        // cache arrays for scaning, to be used later
+        final char[][] uriChars = new char[cnt][];
 
-    //calc the length of output
-    for (int i=0; i<uris.length; i++) {
-      if (uris[i]==null) {
-        throw new NullPointerException("URI at index "+ i +" cannot be a null value");
-      }else {
-        final int len = uris[i].length();
-        charCnt+=len;
-        for (int j=0; j<len; j++) {
-          if (uris[i].charAt(j) == separator) charCnt++;
-        }
-      }
-    }
+        // calculate the maximum length of the output buffer
+        for (int i = 0; i < uris.length; i++) {
+            final String uri = uris[i];
+            if (uri == null) throw new NullPointerException(
+                    "URI at index " + i + " was a null value");
 
-    char[] allChars = new char[charCnt];
-    int charPos=0;
+            final char[] chars = uri.toCharArray();
+            uriChars[i] = chars;
 
-    for (int i=0; i<uris.length; i++) {
-        for (int j=0; j<uris[i].length(); j++) {
-          final char c = uris[i].charAt(j);
-          if (c==separator) {
-            //separator found
-            allChars[charPos++]=c;
-          }
-          allChars[charPos++]=c;
-        }
-        if (i<uris.length-1) allChars[charPos++]=separator;
-    }
-
-    return String.valueOf(allChars);
-  }
-
-  public static final String buildSimpleUriList(final String uris[]) {
-
-    if (uris == null) return null;
-    if (uris.length==0) throw new IllegalArgumentException("URI list cannot be empty");;
-
-    int charCnt = 2*uris.length + uris.length - 1; //number of '' and ,
-
-    //calc the length of output
-    for (int i=0; i<uris.length; i++) {
-      if (uris[i]==null) {
-        throw new NullPointerException("URI at index "+ i +" cannot be a null value");
-      }else {
-        final int len = uris[i].length();
-        charCnt+=len;
-        for (int j=0; j<len; j++) {
-          char c =uris[i].charAt(j);
-          if (c == '/') {
-            if (j<len-1) {
-              charCnt--;
-              j++; // skip next char if not last in uri
-            } else throw new IllegalArgumentException("URI at index "+ i +" must not end with '/'");
-          } else if (c == '\'') charCnt++;
-        }
-      }
-    }
-
-    char[] allChars = new char[charCnt];
-    int charPos=0;
-
-    for (int i=0; i<uris.length; i++) {
-      allChars[charPos++]='\''; //open with '
-      final int len = uris[i].length();
-      for (int j=0; j<len; j++) {
-        final char c = uris[i].charAt(j);
-        allChars[charPos++]=c;
-        if (c == '/') {
-          j++; // contract // to /
-        } else if (c == '\'') allChars[charPos++]=c; // duplicate '
-      }
-      allChars[charPos++]='\''; //close with '
-      if (i < uris.length-1) allChars[charPos++]=','; // add , if needed
-    }
-
-    return String.valueOf(allChars);
-  }
-
-
-  public static final String buildCompositeUriList(final String uris[]) {
-
-    if (uris == null) return null;
-    if (uris.length==0) throw new IllegalArgumentException("URI list cannot be empty");;
-
-    // counting and validation
-    int charCnt = 4*uris.length + uris.length - 1; // number of ('') and ,
-
-    for (int i=0; i<uris.length; i++) {
-      final String uri = uris[i];
-      if (uri==null) {
-        throw new NullPointerException("URI at index "+ i +" cannot be a null value");
-      }else {
-        final int len = uris[i].length();
-        charCnt+=len;
-        for (int j=0; j<len; j++) {
-          final char c = uri.charAt(j);
-          if (c == '/') {
-            if (j<len-1) { // if not last character in string
-              if (uri.charAt(j+1) == '/') {
-                // contract // to /
-                charCnt-=3; // OVO MI SMRDI! trebalo bi biti -1 a ne -3 jer micemo samo taj jedan viska
-              } else {
-                // add ',' instead of /
-                charCnt+=2;
-              }
-            } else {
-              // add ',' if / at end
-              charCnt+=2;
+            cnt += chars.length;
+            for (final char ch: chars) {
+                if (ch == separator) cnt++;
             }
-          } else if (c == '\'') {
-            // expant ' to ''
-            charCnt++;
-          }
         }
-      }
-    }
 
-    //build string
-    char[] allChars = new char[charCnt];
-    int charPos=0;
+        // output buffer
+        final char[] joined = new char[cnt];
+        int pos = 0;
 
-    for (int i=0; i<uris.length; i++) {
-
-      // open with ('
-      allChars[charPos++]='(';
-      allChars[charPos++]='\'';
-      final String uri = uris[i];
-      final int len = uri.length();
-
-      for (int j=0; j<len; j++) {
-        final char c = uri.charAt(j);
-        if (c == '/') {
-          if (j<len-1) { // if not last character in string
-            if (uri.charAt(j+1) == '/') {
-              // contract // to /
-              allChars[charPos++]='/';
-              j++;
-            } else {
-              // add ',' instead of /
-              allChars[charPos++]='\'';
-              allChars[charPos++]=',';
-              allChars[charPos++]='\'';
+        for (final char[] chars : uriChars) {
+            for (final char ch: chars) {
+                if (ch == separator) joined[pos++] = ch;
+                joined[pos++] = ch;
             }
-          } else {
-            // add ',' if / at end
-            allChars[charPos++]='\'';
-            allChars[charPos++]=',';
-            allChars[charPos++]='\'';
-          }
-        } else if (c == '\'') {
-          // expand ' to ''
-          allChars[charPos++]=c;
-          allChars[charPos++]=c;
-        } else {
-          // use character
-          allChars[charPos++]=c;
+            joined[pos++] = separator;
         }
-      }
-      //close with ')
-      allChars[charPos++]='\'';
-      allChars[charPos++]=')';
-      if (i < uris.length-1) allChars[charPos++]=','; // add , if needed
+
+        return new String(joined, 0, pos - 1);
     }
 
-    return String.valueOf(allChars);
-  }
+    public static final String escaping(
+            final String uris[],
+            final char skipMarker,
+            final char quote,
+            final char separator) {
+
+        // escaping is a stable function
+        if (uris == null) return null;
+
+        final int ulen = uris.length;
+        if (ulen == 0) throw new IllegalArgumentException("URI list cannot be empty");
+
+        // cache arrays for scaning, to be used later
+        final char[][] uriChars = new char[ulen][];
+
+        // calculate the maximum length of the output buffer
+        int cnt = (ulen << 1) + ulen; // number of '' and ,
+        for (int i = 0; i < ulen; i++) {
+            final String uri = uris[i];
+            if (uri == null) throw new NullPointerException(
+                    "URI at index " + i + " was a null value");
+
+            final char[] chars = uris[i].toCharArray();
+            uriChars[i] = chars;
+
+            final int len = chars.length;
+            cnt += len;
+
+            for (int j = 0; j < len; j++) {
+                final char ch = chars[j];
+                if (ch == skipMarker) {
+                    if (j < len - 1) {
+                        cnt--;
+                        j++; // skip next char if not last in the URI
+                    } else throw new IllegalArgumentException(
+                            "URI at index " + i + " contained a single " +
+                            skipMarker + " at the end");
+                } else if (ch == quote) {
+                    cnt++;
+                }
+            }
+        }
+
+        // output buffer
+        final char[] joined = new char[cnt];
+        int pos = 0;
+
+        for (final char[] chars : uriChars) {
+            joined[pos++] = quote; // open with '
+            final int len = chars.length;
+            for (int j = 0; j < len; j++) {
+                final char ch = chars[j];
+                joined[pos++] = ch;
+                if (ch == skipMarker) {
+                    j++; // contract // to /
+                } else if (ch == quote) {
+                    joined[pos++] = ch; // duplicate '
+                }
+            }
+
+            joined[pos++] = quote; // close with '
+            joined[pos++] = separator; // add , if needed
+        }
+
+        return new String(joined, 0, pos - 1);
+    }
+
+    public static final String escaping(
+            final String uris[],
+            final char skipNext,
+            final char quote,
+            final char separator,
+            final char open,
+            final char close) {
+
+        // escaping is a stable function
+        if (uris == null) return null;
+
+        final int ulen = uris.length;
+        if (ulen == 0) throw new IllegalArgumentException("URI list cannot be empty");
+
+        // cache arrays for scaning, to be used later
+        final char[][] uriChars = new char[ulen][];
+
+        // calculate the maximum length of the output buffer
+        int cnt = (ulen << 2) + ulen; // number of ('') and ,
+        for (int i = 0; i < ulen; i++) {
+            final String uri = uris[i];
+            if (uri == null) throw new NullPointerException(
+                    "URI at index " + i + " was a null value");
+
+            final char[] chars = uris[i].toCharArray();
+            uriChars[i] = chars;
+
+            final int len = chars.length;
+            cnt += len;
+
+            for (int j = 0; j < len; j++) {
+                final char ch = chars[j];
+                if (ch == skipNext) {
+                    if (j < len - 1) { // if not last character in string
+                        if (chars[j + 1] == skipNext) {
+                            cnt -= 3; // contract // to /
+                        } else {
+                            cnt += 2; // add separator instead of /
+                        }
+                    } else {
+                        cnt += 2; // add separator if / at end
+                    }
+                } else if (ch == quote) {
+                    cnt++; // expand ' to ''
+                }
+            }
+        }
+
+        // output buffer
+        final char[] joined = new char[cnt];
+        int pos = 0;
+
+        for (int i = 0; i < ulen; i++) {
+            // open with ('
+            joined[pos++] = open;
+            joined[pos++] = quote;
+
+            final char[] chars = uriChars[i];
+            final int len = chars.length;
+
+            for (int j = 0; j < len; j++) {
+                final char ch = chars[j];
+                if (ch == skipNext) {
+                    if (j < len - 1) { // if not last character in string
+                        if (chars[j + 1] == skipNext) {
+                            // contract // to /
+                            joined[pos++] = skipNext;
+                            j++;
+                        } else {
+                            // add separator instead of /
+                            joined[pos++] = quote;
+                            joined[pos++] = separator;
+                            joined[pos++] = quote;
+                        }
+                    } else {
+                        // add separator if / at end
+                        joined[pos++] = quote;
+                        joined[pos++] = separator;
+                        joined[pos++] = quote;
+                    }
+                } else if (ch == quote) {
+                    // expand ' to ''
+                    joined[pos++] = ch;
+                    joined[pos++] = ch;
+                } else {
+                    // use character
+                    joined[pos++] = ch;
+                }
+            }
+            // close with ')
+            joined[pos++] = quote;
+            joined[pos++] = close;
+            joined[pos++] = separator; // add , if needed
+        }
+
+        return new String(joined, 0, pos - 1);
+    }
 }
---
+++ util/src/test/scala/io/jvm/test/JoinFeatureSpec.scala
@@ -0,0 +1,144 @@
+package io.jvm
+package test
+
+import org.scalatest._
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class JoinFeatureSpec extends FeatureSpec with GivenWhenThen with MustMatchers {
+  feature("jvm.io.Join.escaping()"){
+    def testBuildURI(uris: Array[String], separator: Character) =
+      uris.map(_ replace(separator.toString, separator.toString * 2)).mkString(separator.toString)
+
+    scenario("Escaping is a stable function") {
+      Given("a null input string")
+      When("it is escaped via a character")
+      Then("a null string must be returned")
+      Join.escaping(null, 'x') must equal (null)
+    }
+
+    scenario("Escaping an array contains null") {
+      intercept[NullPointerException] {
+        Join.escaping(Array(null, "fx"), 'x')
+      }
+    }
+
+    scenario("Test 3 : array argument does not contain separator") {
+      Join.escaping(Array("ab", "f"), 'x') must equal ("""abxf""")
+    }
+
+    scenario("Test 4 : array argument size = 0") {
+      intercept[IllegalArgumentException] {
+        Join.escaping(Array(), 'x')
+      }
+    }
+
+    scenario("Test 5 : normal array argument") {
+      Join.escaping(Array("axb", "fx"), 'x') must equal ("""axxbxfxx""")
+    }
+
+    scenario("Test 6 : array argument contains an empty string") {
+      Join.escaping(Array("", "fx"), 'x') must equal ("""xfxx""")
+    }
+
+    scenario("Test 7 : Joining with a regular expression special character") {
+      Given("a string consisting of some special characters")
+      When("it is escaped via that character")
+      Then("the special characters must be duplicated")
+      Join.escaping(Array("a+b","c+d"), '+') must equal ("""a++b+c++d""")
+    }
+  }
+
+  feature("jvm.io.buildSimpleUriList()") {
+    def buildSimpleUriList = Join.escaping(_: Array[String], '/', '\'', ',');
+
+    scenario("1 : null array argument") {
+      buildSimpleUriList(null) must equal (null)
+    }
+
+    scenario("2 : empty array") {
+      intercept[IllegalArgumentException] {
+        buildSimpleUriList(Array())
+      }
+    }
+
+    scenario("3 : array contains null") {
+      intercept[NullPointerException] {
+        buildSimpleUriList(Array("",null))
+      }
+    }
+
+    scenario("4 : array contains empty string") {
+        buildSimpleUriList(Array("","//","'")) must equal ("""'','/',''''""")
+    }
+
+    scenario("5 : simple build uri test without special chars") {
+      buildSimpleUriList(Array("ab","cd")) must equal ("""'ab','cd'""")
+    }
+
+    scenario("6 : simple join uri test with both special chars") {
+      buildSimpleUriList(Array("a'b","c//d")) must equal ("""'a''b','c/d'""")
+    }
+
+    scenario("7 : test a//b , a'//b , 'ab// , //ab', // ") {
+      buildSimpleUriList(Array("a//b","a'//b","'ab//","//ab'","//")) must equal ("""'a/b','a''/b','''ab/','/ab''','/'""")
+    }
+  }
+
+  feature("jvm.io.buildCompositeUriList()") {
+    def buildCompositeUriList = Join.escaping(_: Array[String], '/', '\'', ',', '(', ')')
+
+    scenario("1 : null array argument") {
+      buildCompositeUriList(null) must equal (null)
+    }
+
+    scenario("2 : empty array") {
+      intercept[IllegalArgumentException] {
+        buildCompositeUriList(Array())
+      }
+    }
+
+    scenario("3 : array contains null") {
+      intercept[NullPointerException] {
+        buildCompositeUriList(Array("",null))
+      }
+    }
+    scenario("4.1 : array contains empty string") {
+      buildCompositeUriList(Array("")) must equal ("""('')""")
+    }
+
+    scenario("4.2 : array contains ' character") {
+      buildCompositeUriList(Array("a'b")) must equal ("""('a''b')""")
+    }
+
+    scenario("4.3 : array contains // characters") {
+      buildCompositeUriList(Array("a//b")) must equal ("""('a/b')""")
+    }
+
+    scenario("4.4 : array contains / character") {
+      buildCompositeUriList(Array("a/b")) must equal ("""('a','b')""")
+    }
+
+    scenario("5 : simple test without special chars") {
+      buildCompositeUriList(Array("ab","cd")) must equal ("""('ab'),('cd')""")
+    }
+
+    scenario("6 : simple test with special chars") {
+      buildCompositeUriList(Array("a'b","c//d","e/f")) must equal ("""('a''b'),('c/d'),('e','f')""")
+    }
+
+    scenario("7 : test / at both beginning and end, // and /'/ ") {
+      buildCompositeUriList(Array("/a//b//c/","/'/")) must equal ("""('','a/b/c',''),('','''','')""")
+    }
+
+    scenario("8 : more complex test ") {
+      buildCompositeUriList(Array("a//b","//","/c//d/","/")) must equal ("""('a/b'),('/'),('','c/d',''),('','')""")
+    }
+
+    scenario("9 : //a// and /a/ and 'a' ") {
+      buildCompositeUriList(Array("//a//","/a/","'a'")) must equal ("""('/a/'),('','a',''),('''a''')""")
+    }
+  }
+}
