c239d33ce38f2ec7891b380d49c5b6daf1adae5e
hperadin <hperadin@element.hr>
2014-03-21 10:38:02 +0100

    Strict Json testing

--- json/src/test/java/io/jvm/json/Json2XmlRoundTripTest.java
+++ json/src/test/java/io/jvm/json/Json2XmlRoundTripTest.java
@@ -94,7 +94,7 @@ public class Json2XmlRoundTripTest {
     System.out.println(lhs);
     System.out.println(rhs);
     System.out.println();
-    JSONAssert.assertEquals(lhs, rhs, false);
+    JSONAssert.assertEquals(lhs, rhs, true);
   }
 
   private static void assertXmlEquivalence(String message, Document lhs, Document rhs) {
